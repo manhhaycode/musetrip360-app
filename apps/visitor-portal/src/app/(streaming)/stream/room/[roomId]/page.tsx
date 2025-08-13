d717560fec3219d31775de2bfa48a35023554595
manhhaycode <107235653+manhhaycode@users.noreply.github.com>
2025-08-13 22:16:30 +0700

    fix: hot fix core package

--- apps/visitor-portal/src/app/(streaming)/stream/room/[roomId]/page.tsx
+++ apps/visitor-portal/src/app/(streaming)/stream/room/[roomId]/page.tsx
@@ -3,8 +3,10 @@
 import { useStreamingContext } from '@musetrip360/streaming/contexts';
 import { StreamingRoom } from '@musetrip360/streaming/ui';
 
-import { useVirtualTourById, VirtualTourViewer } from '@musetrip360/virtual-tour';
-import { use, useEffect } from 'react';
+import { useVirtualTourById } from '@musetrip360/virtual-tour';
+import { Suspense, use, useEffect } from 'react';
+
+const VirtualTourViewer = await import('@musetrip360/virtual-tour').then((mod) => mod.VirtualTourViewer);
 
 interface StreamRoomPageProps {
   params: Promise<{ roomId: string }>;
@@ -27,7 +29,11 @@ export default function StreamRoomPage({ params }: StreamRoomPageProps) {
       {isLoading || !virtualTour ? (
         <div className="flex items-center justify-center h-full text-primary">Loading...</div>
       ) : (
-        <VirtualTourViewer virtualTour={virtualTour} />
+        <Suspense
+          fallback={<div className="flex items-center justify-center h-full text-primary">Loading Virtual Tour...</div>}
+        >
+          <VirtualTourViewer virtualTour={virtualTour} />
+        </Suspense>
       )}
     </StreamingRoom>
   );
--- packages/user-management/lib/state/context/UserProvider.tsx
+++ packages/user-management/lib/state/context/UserProvider.tsx
@@ -3,38 +3,49 @@ import { useEffect, useState } from 'react';
 import { Fragment } from 'react/jsx-runtime';
 import { useUserStore } from '../store/user.store';
 import { IUser } from '@/types';
-// import { getQueryClient } from '@musetrip360/query-foundation';
-// import { userCacheKeys } from '@/api/cache/cacheKeys';
 import { useIsAuthenticated } from '@musetrip360/auth-system/state';
+import { useShallow } from 'zustand/shallow';
+import { getQueryClient } from '@musetrip360/query-foundation';
+import { userCacheKeys, UserCacheKeys } from '@/api/cache/cacheKeys';
 
-export const UserProvider = ({ children }: { children: React.ReactNode }) => {
+export const UserProvider = ({ children, strictMode = true }: { children: React.ReactNode; strictMode: boolean }) => {
   const [isHydrated, setIsHydrated] = useState(false);
+  const { hydrate, setUser } = useUserStore(
+    useShallow((state) => ({ hydrate: state.hydrate, setUser: state.setUser }))
+  );
   const isAuthenticated = useIsAuthenticated();
-  const { data: user } = useCurrentProfile({
+  const { data: user, refetch } = useCurrentProfile({
     enabled: isHydrated && isAuthenticated,
   });
 
-  useEffect(() => {
-    const hydrate = async () => {
-      await useUserStore.persist.rehydrate();
-      setIsHydrated(true);
-    };
-    hydrate();
-  }, []);
+  console.log(user);
 
-  // useEffect(() => {
-  //   if (!isAuthenticated && isHydrated) {
-  //     getQueryClient().removeQueries({ queryKey: userCacheKeys.profile() });
-  //     useUserStore.getState().resetStore();
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [isAuthenticated]);
+  useEffect(() => {
+    hydrate().then(() => {
+      setIsHydrated(true);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
     if (user) {
-      useUserStore.setState({ user: user as IUser });
+      setUser(user);
     }
-  }, [user]);
+  }, [user, setUser]);
 
-  return <Fragment>{isHydrated ? children : null}</Fragment>;
+  useEffect(() => {
+    if (isHydrated && isAuthenticated) {
+      getQueryClient().invalidateQueries({ queryKey: userCacheKeys.profile() });
+      refetch();
+    }
+    if (isHydrated && !isAuthenticated) {
+      getQueryClient().removeQueries({ queryKey: userCacheKeys.profile() });
+    }
+  }, [isHydrated, isAuthenticated, refetch]);
+
+  if (strictMode) {
+    return <Fragment>{isHydrated ? children : null}</Fragment>;
+  }
+
+  return children;
 };
