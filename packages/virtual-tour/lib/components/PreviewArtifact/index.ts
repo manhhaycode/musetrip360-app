0b9b5f2654cd4c23675c1264bc03f2ead03f27dd
manhhaycode <107235653+manhhaycode@users.noreply.github.com>
2025-08-20 16:33:22 +0700

    feat: create preview artifact 3dmodel for virtual tour

---
+++ packages/virtual-tour/lib/components/PreviewArtifact/index.ts
@@ -0,0 +1,11 @@
+'use client';
+
+export { PreviewArtifact } from './PreviewArtifact';
+export { ControlBar } from './ControlBar';
+export { InfoPanel } from './InfoPanel';
+export { ImagesPanel } from './ImagesPanel';
+
+export type { PreviewArtifactProps } from './PreviewArtifact';
+export type { ControlBarProps } from './ControlBar';
+export type { InfoPanelProps } from './InfoPanel';
+export type { ImagesPanelProps } from './ImagesPanel';
